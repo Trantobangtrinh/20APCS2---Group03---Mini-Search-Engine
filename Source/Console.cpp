acb9ca880de06d722fd7b86f32f2e2a596bff8f3
khangdeptraii02 <phanvinhkhang910@gmail.com>
2021-07-11 15:54:28 +0700

    update interface in main.cpp

---
+++ Source/Console.cpp
@@ -0,0 +1,38 @@
+#include "Console.h"
+
+void fixConsoleWindow() {
+	HWND consoleWindow = GetConsoleWindow();
+	LONG style = GetWindowLong(consoleWindow, GWL_STYLE);
+	style = style & ~(WS_MAXIMIZEBOX) & ~(WS_THICKFRAME);
+	SetWindowLong(consoleWindow, GWL_STYLE, style);
+}
+
+void setConsoleWindow(int w, int h) {
+	HWND console = GetConsoleWindow();
+	RECT r;
+	GetWindowRect(console, &r);
+
+	MoveWindow(console, r.left, r.top, w, h, TRUE);
+}
+
+void gotoXY(int x, int y) {
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+void Textcolor(int color)
+{
+	HANDLE hConsoleColor;
+	hConsoleColor = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsoleColor, color);
+}
+
+void AnTroChuot()
+{
+	CONSOLE_CURSOR_INFO Info;
+	Info.bVisible = FALSE;
+	Info.dwSize = 20;
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &Info);
+}
---
+++ Source/Console.h
@@ -0,0 +1,42 @@
+#ifndef _CONSOLE_H_
+#define _CONSOLE_H_
+
+#include <Windows.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <mmsystem.h>
+
+#define SIZE			16  
+#define Black			0
+#define DarkBlue		1
+#define DarkGreen		2
+#define DarkCyan		3
+#define DarkRed			4
+#define DarkPink		5
+#define DarkYellow		6
+#define DarkWhite		7
+#define Grey			8
+#define Blue			9
+#define Green			10
+#define Cyan			11
+#define Red				12
+#define Pink			13
+#define Yellow			14
+#define White			15
+#define defaultColor	7 
+
+// Function to set the size of the console
+void setConsoleWindow(int w, int h);
+
+// Function to lock the size of the console
+void fixConsoleWindow();
+
+// Function to coloring text
+void Textcolor(int color);
+
+// Function to move cursor 
+void gotoXY(int x, int y);
+
+// Function to hide the cursor
+void AnTroChuot();
+#endif
