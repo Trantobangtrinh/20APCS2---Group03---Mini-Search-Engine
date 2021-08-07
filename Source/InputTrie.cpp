8eddb2b343f64d6acd09fab1d9889c3657a28a35
Trantobangtrinh <20125053@student.hcmus.edu.vn>
2021-08-07 17:02:30 +0700

    Update InputTrie.cpp

--- Source/InputTrie.cpp
+++ Source/InputTrie.cpp
@@ -5,13 +5,14 @@ int convertKey(char key) {
     if (key >= 97 && key <= 122) return (int(key) - 87);
     if (key >= 65 && key <= 90) return (int(key) - 55);
 
-    if (key == '$') return 36;
-    if (key == '.') return 37;
-    if (key == '#') return 38;
-    if (key == '-') return 39;
-    if (key == '\'') return 40;
-    if (key == ',') return 41;
-    
+    if (key == '#') return 36;
+    if (key == '$') return 37;
+    if (key == '%') return 38;
+    if (key == '.') return 39;
+    if (key == ',') return 40;
+    if (key == '\'') return 41;
+    if (key == '-') return 42;
+
     return -1;
 }
 
@@ -25,14 +26,15 @@ void saveTrie(trieNode*& root, trieNode* &rootTitle, unordered_set<string> stopW
     stringstream ss(word);
     while (ss >> word) {
         isTit = true;
-        inputData(rootTitle, stopWord, number, word, path, i, isTit);
         i++;
+        inputData(rootTitle, stopWord, number, word, path, i, isTit);
     }
 
     while (in >> word) {
         isTit = false;
-        inputData(root, stopWord,number, word, path, i, isTit);
         i++;
+        inputData(root, stopWord,number, word, path, i, isTit);
+        
     }
     in.close();
 
@@ -53,7 +55,6 @@ void getStopWord(unordered_set <string> &stopWord, string path) {
 void inputData(trieNode* &root, unordered_set<string> stopWord, vector<float>&number, string word, string path, int position, bool isTit) {
     trieNode* cur = root;
     bool check = false;
-    vector<float>::iterator itr;
     // Check từ trong stop word
     // Nếu từ đó không phải stop word thì insert vào Trie
     if (isTit == true || stopWord.find(word) == stopWord.end()) {
@@ -61,20 +62,22 @@ void inputData(trieNode* &root, unordered_set<string> stopWord, vector<float>&nu
         if (word[0] == '$') {
             size_t pos = word.length() - 1;
             string tmp = word.substr(1, pos);
-
-            itr = find(number.begin(), number.end(), stof(tmp));
-            if (itr == number.end())
-                number.push_back(stof(tmp));
+            number.push_back(stof(tmp));
         }
+
         for (int i = 0; i < word.length(); ++i) {
+            cleanData(word);
             int index = convertKey(word[i]);
             if (index == -1)
                 continue;
             if ((i == word.length() - 2) && convertKey(word[i + 1]) >= 36) {
+                cur->child[index] = new trieNode();
+                cur = cur->child[index];
                 break;
             }
             if (cur->child[index] == nullptr)
                 cur->child[index] = new trieNode();
+
             cur = cur->child[index];
         }
 
@@ -109,22 +112,46 @@ vector<Store> search(trieNode* root, string word) {
         int index = convertKey(word[i]);
         if (index == -1)
             continue;
-        if (!cur->child[index] && !cur->isLeaf) {
+        if (!cur->child[index]) {
             vector<Store> null;
             return null;
         }
-        if (!cur->isLeaf)
-            cur = cur->child[index];
+        cur = cur->child[index];
     }
-
-    return cur->file;
+    if (cur->isLeaf) {
+        return cur->file;
+    }
+        
+    else {
+        vector<Store> null;
+        return null;
+    }
 }
 
 void readFile(trieNode*& root, trieNode* &rootTitle, unordered_set<string> stopWords, vector<float> &number) {
-    for (int i = 96; i <=100; ++i) {
+    for (int i = 96; i <= 100; ++i) {
         string tmp = to_string(i) + ".txt";
         saveTrie(root, rootTitle, stopWords, number, tmp);
         cout << "\r" << i << " %";
         
+    }
+}
+
+void cleanData(string& word) {
+    for (int i = 0; i < word.size(); i++) {
+        int idx = convertKey(word[i]);
+        if (idx == -1 || (idx >= 36 && idx <= 37 && i != 0)) {
+            word.erase(i, 1);
+            --i;
+        }
+    }
+
+    if (word.size() == 1 && convertKey(word[0]) >= 36)
+        word.erase(0, 1);
+
+    int i = word.size() - 1;
+    if (i >= 0 && (convertKey(word[i]) >= 38 && convertKey(word[i]) <= 40)) {
+        word.erase(i, 1);
+        //i = word.size() - 1;
     }
 }
